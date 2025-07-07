f9c360353b6ee87a585030b71bb1dd5e2f301673
Anup Dahal <adahal39@gmail.com>
2025-07-07 20:35:04 +0545

    Fix: changes in whitespace

--- backend/app/worker/photogrammetry/images_to_point_cloud.py
+++ backend/app/worker/photogrammetry/images_to_point_cloud.py
@@ -330,7 +330,7 @@ def run(process_dir, config):
             preview_pcd = cropped_pcd.random_down_sample(sampling_ratio)
     else:
         preview_pcd = cropped_pcd
-        
+
     preview_ply = os.path.join(process_dir, 'undistorted', 'depthmaps', 'merged_preview.ply')
     o3d.io.write_point_cloud(preview_ply, preview_pcd, write_ascii=True, compressed=False, print_progress=True)
 
